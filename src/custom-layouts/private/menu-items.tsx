bfef0fc3698c43f49387f58468f93ac7984ca16b
nimavisker1360 <nimavisker@gmail.com>
2025-02-27 11:37:53 +0300

    Complete MenuHamburger

--- src/custom-layouts/private/menu-items.tsx
+++ src/custom-layouts/private/menu-items.tsx
@@ -8,39 +8,49 @@ import {
   LayoutDashboard,
   ListStart,
 } from "lucide-react";
+import { usePathname } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 interface MenuItemsProps {
   showMenuItems: boolean;
   setShowMenuItems: (showMenuItems: boolean) => void;
 }
 const MenuItems = ({ showMenuItems, setShowMenuItems }: MenuItemsProps) => {
-    const iconSize = 16;
-    const menuItems = [
-      {
-        name: "Dashboard",
-        icon: <LayoutDashboard size={iconSize} />,
-      },
-      {
-        name: "Doctors",
-        icon: <GraduationCap size={iconSize} />,
-      },
-      {
-        name: "Appointments",
-        icon: <CalendarClock size={iconSize} />,
-      },
-      {
-        name: "Patients",
-        icon: <Contact size={iconSize} />,
-      },
-      {
-        name: "Reports",
-        icon: <Banknote size={iconSize} />,
-      },
-      {
-          name: "Staff / Users",
-          icon: <ListStart  size={iconSize} />,
-        },
-    ];
+  const iconSize = 16;
+  const pathname = usePathname();
+  const router = useRouter();
+  const menuItems = [
+    {
+      name: "Dashboard",
+      icon: <LayoutDashboard size={iconSize} />,
+      path: "/admin/dashboard",
+    },
+    {
+      name: "Doctors",
+      icon: <GraduationCap size={iconSize} />,
+      path: "/admin/doctors",
+    },
+    {
+      name: "Appointments",
+      icon: <CalendarClock size={iconSize} />,
+      path: "/admin/appointments",
+    },
+    {
+      name: "Patients",
+      icon: <Contact size={iconSize} />,
+      path: "/admin/patients",
+    },
+    {
+      name: "Reports",
+      icon: <Banknote size={iconSize} />,
+      path: "/admin/reports",
+    },
+    {
+      name: "Staff / Users",
+      icon: <ListStart size={iconSize} />,
+      path: "/admin/staff",
+    },
+  ];
 
   return (
     <Drawer
@@ -48,14 +58,25 @@ const MenuItems = ({ showMenuItems, setShowMenuItems }: MenuItemsProps) => {
       onClose={() => setShowMenuItems(false)}
       title="Menu Items"
     >
-        <div className="flex flex-col gap-7">
-            {menuItems.map((item, index) => (
-                <div key={index} className="flex gap-5 items-center">
-                    {item.icon}
-                    <span>{item.name}</span>
-                </div>
-            ))}
-        </div>
+      <div className="flex flex-col gap-10  mt-10">
+        {menuItems.map((item, index) => (
+          <div
+            key={index}
+            onClick={() => {
+              router.push(item.path);
+              setShowMenuItems(false);
+            }}
+            className={`p-2 flex gap-5 items-center cursor-pointer ${
+              pathname === item.path
+                ? "border-primary bg-gray-200 border-solid border"
+                : ""
+            }`}
+          >
+            {item.icon}
+            <span className="text-sm">{item.name}</span>
+          </div>
+        ))}
+      </div>
     </Drawer>
   );
 };
